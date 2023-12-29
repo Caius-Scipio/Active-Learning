4995c9135675e12c22151f7b27430e4b7f631ae1
Caius Scipio <caiuscipio@gmail.com>
2023-12-29 10:41:11 +0900

    JS Basic question 124-133

--- JS/script_basic3.js
+++ JS/script_basic3.js
@@ -12,8 +12,7 @@ function question102(array) {
   let counter = 0;
   for (let i = 0; i < array.length; i++) {
     for (let j = i + 1; j < array.length; j++) {
-      if (array[i] > array[j])
-        counter++;
+      if (array[i] > array[j]) counter++;
     }
   }
   return counter;
@@ -23,7 +22,8 @@ console.log(question102([0, 3, 2, 5, 9]));
 // 103. Write a JavaScript program to find the maximum number of a given positive integer by deleting exactly one digit of the given number.
 console.log("%cJavaScript Basic #93", "color: red");
 function question103(num) {
-  let result = 0, numbers = [];
+  let result = 0,
+    numbers = [];
   while (num) {
     numbers.push(num % 10);
     num = Math.floor(num / 10);
@@ -105,7 +105,7 @@ function question107(array) {
   for (let i = 0; i < array.length; i++) {
     for (let j = i + 1; j < array.length; j++) {
       if (array[i] % array[j] === 0 || array[j] % array[i] === 0) {
-        result++
+        result++;
       }
     }
   }
@@ -154,7 +154,7 @@ function question110(array, num) {
   let count = 0;
   for (let i = 0; i < limit; i++) {
     if (array[i] % 2 === 0) {
-      count++
+      count++;
     }
   }
   return count;
@@ -186,7 +186,7 @@ function question112(num) {
     let n = i;
     while (n % 5 === 0) {
       n /= 5;
-      result++
+      result++;
     }
   }
   return result;
@@ -209,7 +209,7 @@ console.log(question113(26));
 console.log("%cJavaScript Basic #114", "color: red");
 function question114(str) {
   let regex = /^[A-Z].*\.$/;
-  return regex.test(str)
+  return regex.test(str);
 }
 console.log(question114(`This is correct.`));
 console.log(question114(`this is incorrect`));
@@ -229,14 +229,26 @@ function question115(array) {
   }
   return true;
 }
-console.log(question115([[1, 0, 0], [0, 2, 0], [0, 0, 3]]));
-console.log(question115([[1, 0, 0], [0, 2, 3], [0, 0, 3]]));
+console.log(
+  question115([
+    [1, 0, 0],
+    [0, 2, 0],
+    [0, 0, 3],
+  ])
+);
+console.log(
+  question115([
+    [1, 0, 0],
+    [0, 2, 3],
+    [0, 0, 3],
+  ])
+);
 //
 // 116. Write a JavaScript program to find all the possible options to replace the hash in a string (Consists of digits and one hash (#)) with a digit to produce an integer divisible by 3.
 // For a string "2*0", the output should be : ["210", "240", "270"]
 console.log("%cJavaScript Basic #116", "color: red");
 function question116(str) {
-  let result = []
+  let result = [];
   for (let i = 0; i <= 9; i++) {
     let newStr = str.replace(/#/, i);
     if (newStr % 3 === 0) {
@@ -256,15 +268,27 @@ console.log("%cJavaScript Basic #117", "color: red");
 function question117(array) {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length; j++) {
-      if (i === j && array[i][j] !== 1 || array[i][j] && i !== j) {
+      if ((i === j && array[i][j] !== 1) || (array[i][j] && i !== j)) {
         return false;
       }
     }
   }
   return true;
 }
-console.log(question117([[1, 0, 0], [0, 1, 0], [0, 0, 1]]));
-console.log(question117([[1, 0, 0], [0, 1, 0], [1, 0, 1]]));
+console.log(
+  question117([
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1],
+  ])
+);
+console.log(
+  question117([
+    [1, 0, 0],
+    [0, 1, 0],
+    [1, 0, 1],
+  ])
+);
 //
 // 118. Write a JavaScript program to check whether a given number is in a given range.
 console.log("%cJavaScript Basic #118", "color: red");
@@ -294,7 +318,7 @@ console.log(question119(123));
 // Point inside a circle (a, b)
 console.log("%cJavaScript Basic #120", "color: red");
 function question120(centerX, centerY, radius, pointX, pointY) {
-  return Math.sqrt(Math.pow(pointX-centerX, 2) + Math.pow(pointY-centerY, 2)) < radius;
+  return Math.sqrt(Math.pow(pointX - centerX, 2) + Math.pow(pointY - centerY, 2)) < radius;
 }
 console.log(question120(0, 0, 6, 2, 4));
 //
@@ -311,7 +335,13 @@ function question121(array) {
   }
   return true;
 }
-console.log(question121([[1, 0, 0],[2, 0, 0],[0, 3, 3],]));
+console.log(
+  question121([
+    [1, 0, 0],
+    [2, 0, 0],
+    [0, 3, 3],
+  ])
+);
 //
 // 122. Write a JavaScript program to check whether a given array of integers represents a strictly increasing or decreasing sequence.
 console.log("%cJavaScript Basic #122", "color: red");
@@ -324,8 +354,8 @@ function question122(array) {
   }
   return true;
 }
-console.log(question122([1,2,3]));
-console.log(question122([3,4,1]));
+console.log(question122([1, 2, 3]));
+console.log(question122([3, 4, 1]));
 //
 // 123. Write a JavaScript program to find out if the members of a given array of integers are a permutation of numbers from 1 to a given integer.
 console.log("%cJavaScript Basic #123", "color: red");
@@ -337,40 +367,144 @@ function question123(array, num) {
   }
   return true;
 }
-console.log(question123([1,2,3,4,5], 5));
+console.log(question123([1, 2, 3, 4, 5], 5));
+//
 // 124. Write a JavaScript program to create the NOR value of two given booleans.
 // Note: In boolean logic, logical nor or joint denial is a truth-functional operator which produces a result that is the negation of logical or. That is, a sentence of the form (p NOR q) is true precisely when neither p nor q is true - i.e. when both of p and q are false
 // Sample Example:
 // For x = true and y = false, the output should be logical_Nor(x, y) = false; For x = false and y = false, the output should be logical_Nor(x, y) = true.
-
+console.log("%cJavaScript Basic #124", "color: red");
+function question124(booleanA, booleanB) {
+  return !booleanA && !booleanB;
+}
+console.log(question124(true, false));
+//
 // 125. Write a JavaScript program to find the longest string in a given array.
-
+console.log("%cJavaScript Basic #125", "color: red");
+function question125(array) {
+  return array.sort((a, b) => b.length - a.length)[0];
+}
+console.log(question125(["a", "aa", "aaa", "aaaaa"]));
+//
 // 126. Write a JavaScript program to get the largest even number from an array of integers.
-
+console.log("%cJavaScript Basic #126", "color: red");
+function question126(array) {
+  let maxEven = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0 && array[i] > maxEven) {
+      maxEven = array[i];
+    }
+  }
+  return maxEven;
+}
+console.log(question126([343, 121, 6, 200, 5]));
+//
 // 127. Write a JavaScript program to reverse the order of bits in a integer.
 // 14 -> 00001110 -> 01110000 -> 112
 // 56 -> 00111000 -> 00011100 -> 28
 // 234 -> 11101010 -> 01010111 -> 87
-
+console.log("%cJavaScript Basic #127", "color: red");
+function question127(num) {
+  let binary = num.toString(2).split("");
+  for (let i = 0; i < 8 - binary.length; i++) {
+    binary.unshift("0");
+  }
+  return parseInt(binary.reverse().join(""), 2);
+}
+console.log(question127(14));
+//
 // 128. Write a JavaScript program to find the smallest round number not less than a given value.
 // Note: A round number is informally considered to be an integer that ends with one or more zeros.[3] So, 590 is rounder than 592, but 590 is less round than 600.
-
+console.log("%cJavaScript Basic #128", "color: red");
+function question128(num) {
+  return Math.ceil(num / 10) * 10;
+}
+console.log(question128(56));
+console.log(question128(171));
+//
 // 129. Write a JavaScript program to find the smallest prime number strictly greater than a given number.
-
+console.log("%cJavaScript Basic #129", "color: red");
+function question129(num) {
+    for (let count = num + 1; ; ++count) {
+    let isPrime = true;
+    for (let i = 2; i < Math.sqrt(count) + 1; ++i) {
+      if (count % i === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      return count;
+    }
+  }
+}
+console.log(question129(6));
+//
 // 130. Write a JavaScript program to find the number of even digits in a given integer.
-
+console.log("%cJavaScript Basic #130", "color: red");
+function question130(num) {
+  let array = num.toString().split("");
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(question130(1020));
+//
 // 131. Write a JavaScript program to create an array of prefix sums of the given array.
 // In computer science, the prefix sum, cumulative sum, inclusive scan, or simply scan of a sequence of numbers x0, x1, x2, ... is a second sequence of numbers y0, y1, y2, ..., the sums of prefixes of the input sequence:
 // y0 = x0
 // y1 = x0 + x1
 // y2 = x0 + x1+ x2
 // ...
-
+console.log("%cJavaScript Basic #131", "color: red");
+function question131(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray[i] = 0;
+    for (let j = 0; j < i + 1; j++) {
+      newArray[i] += array[j];
+    }
+  }
+  return newArray;
+}
+console.log(question131([1, 2, 3, 4, 5]));
+//
 // 132. Write a JavaScript program to find all distinct prime factors of a given integer.
+console.log("%cJavaScript Basic #132", "color: red");
+function question132(num) {
+  function is_prime(num) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
 
+  const result = [];
+
+  for (let i = 2; i <= num; i++) {
+    while (is_prime(i) && num % i === 0) {
+      if (!result.includes(i)) result.push(i);
+      num /= i;
+    }
+  }
+
+  return result;
+}
+console.log(question132(100));
+//
 // 133. Write a JavaScript program to check whether a given fraction is proper or not.
 // Note: There are two types of common fractions, proper or improper. When the numerator and the denominator are both positive, the fraction is called proper if the numerator is less than the denominator, and improper otherwise.
-
+console.log("%cJavaScript Basic #133", "color: red");
+function question133(numerator, denominator) {
+  return Math.abs(numerator / denominator) < 1 ? `It's proper` : `It's improper`;
+}
+console.log(question133(1, 2));
+console.log(question133(7, 4));
+//
 // 134. Write a JavaScript program to change the characters (lower case) in a string where a turns into z, b turns into y, c turns into x, ..., n turns into m, m turns into n, ..., z turns into a.
 
 // 135. Write a JavaScript program to remove all characters from a given string that appear more than once.
